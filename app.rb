9e423226ac2591e6237c91af574429b530409a5c
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-04 18:54:09 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do 
+    enable :sessions
+    set :session_secret, "Ricarthofranck25071990"
+  end 
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    session["item"] = params[:item]
+    @session_val = session
+    erb :display
+  end 
 end
